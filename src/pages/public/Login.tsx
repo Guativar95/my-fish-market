de9eabc73e2e45f31317b9111e10a20984b964b6
Brayan Guativa <brian_951022@hotmail.com>
2025-04-30 14:54:17 -0500

    Inir Login

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useState } from 'react';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
-const Login = () => {
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div>Login</div>
-  )
-}
+    <div className="min-h-screen grid bg-gray-100">
+        
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">Iniciar sesión</h2>
 
-export default Login
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 mb-2">Usuario</label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Usuario"
+              required
+            />
+          </div>
+
+          <div className="mb-6 relative">
+            <label htmlFor="password" className="block text-gray-700 mb-2">Contraseña</label>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
+              placeholder="••••••••"
+              required
+            />
+            <div
+              className="absolute right-3 top-10 transform -translate-y-1/2 cursor-pointer text-gray-600"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <EyeSlashIcon className="w-5 h-5" />
+              ) : (
+                <EyeIcon className="w-5 h-5" />
+              )}
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200"
+          >
+            Iniciar sesión
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
